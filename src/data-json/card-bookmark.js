e38b66292bc474b7c1ec8e6bc774ff324685858e
Andreas Bremana <andreasbremana@gmail.com>
2024-05-21 00:09:45 +0700

    File Bookmark

---
+++ src/data-json/card-bookmark.js
@@ -0,0 +1,28 @@
+const bookmark = [
+  {
+    id: 1,
+    url: "/images/mie-aceh.png",
+    title: "Masjid Raya Baiturrahman", // Perbaiki ejaan 'tittle' menjadi 'title'
+    location: "Kec. Baiturrahman, Kota Banda Aceh",
+    typeLocation: "Masjid",
+    typeSellTicket: "Gratis",
+  },
+  {
+    id: 2,
+    url: "/images/pulau-rubiah.png",
+    title: "Pulau Rubiah", // Perbaiki ejaan 'tittle' menjadi 'title'
+    location: "Desa Iboih, Kec. Sukakarya, Kota Sabang",
+    typeLocation: "Pantai",
+    typeSellTicket: "Offline",
+  },
+  {
+    id: 3,
+    url: "/images/mie-aceh.png",
+    title: "Tugu Nol Kilometer", // Perbaiki ejaan 'tittle' menjadi 'title'
+    location: "Desa Iboih, Kec. Sukakarya, Kota Sabang",
+    typeLocation: "Wisata Buatan",
+    typeSellTicket: "Offline",
+  },
+];
+
+export default bookmark;
