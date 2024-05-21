52f32c21fc24213a55dde30ecff51f06eb90b6c8
FakhrulRazi2022 <fahkrulrazy@gmail.com>
2024-05-21 21:03:24 +0700

    memperbaiki panah di festival

--- src/pages/festival/index.jsx
+++ src/pages/festival/index.jsx
@@ -1,10 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import Navigation from "../../components/Navigation";
 import Festival from "../../components/cards/Festival";
 import ArrowRightRegular from "../../assets/icons/arrow-right-regular";
 import ArrowLeftRegular from "../../assets/icons/arrow-left-regular";
 
-export default function index() {
+export default function Index() {
+  // State untuk melacak bulan saat ini
+  const [currentMonth, setCurrentMonth] = useState("April");
+
+  // Fungsi untuk menangani perpindahan ke bulan berikutnya saat tombol panah kanan diklik
+  const handleNextMonth = () => {
+    switch (currentMonth) {
+      case "April":
+        setCurrentMonth("Mei");
+        break;
+      case "Mei":
+        setCurrentMonth("Juni");
+        break;
+      // Tambahkan kasus lainnya untuk bulan-bulan tambahan jika diperlukan
+      default:
+        break;
+    }
+  };
+
+  // Fungsi untuk menangani perpindahan ke bulan sebelumnya saat tombol panah kiri diklik
+  const handlePreviousMonth = () => {
+    switch (currentMonth) {
+      case "Juni":
+        setCurrentMonth("Mei");
+        break;
+      case "Mei":
+        setCurrentMonth("April");
+        break;
+      default:
+        break;
+    }
+  };
+
   return (
     <>
       <Navigation>
@@ -18,12 +50,12 @@ export default function index() {
           />
           <div className="outline outline-2 outline-yellow-primary h-auto rounded-2xl overflow-hidden ">
             <div className="bg-yellow-primary flex justify-between px-10 text-2xl p-3">
-              <h1 className="text-white">Mei 2024</h1>
+              <h1 className="text-white">{currentMonth} 2024</h1>
               <div className="flex gap-3">
-                <button>
+                <button onClick={handlePreviousMonth}>
                   <ArrowLeftRegular />
                 </button>
-                <button className="">
+                <button onClick={handleNextMonth}>
                   <ArrowRightRegular />
                 </button>
               </div>
