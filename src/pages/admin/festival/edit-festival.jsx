3e1db10e3c70d809daa48790153148ad004f9c07
azmi <azmik5671@gmail.com>
2024-06-01 00:49:54 +0800

    fix part of admin

--- src/pages/admin/festival/edit-festival.jsx
+++ src/pages/admin/festival/edit-festival.jsx
@@ -1,37 +1,119 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import useSnackbar from "../../../hooks/useSnackbar";
+import axios from "axios";
 import TextareaAdmin from "../../../components/micro/TextareaAdmin";
 import NavigationAdmin from "../../../components/NavigationAdmin";
 import InputAdmin from "../../../components/micro/InputAdmin";
 import HeadingAdmin from "../../../components/micro/HeadingAdmin";
 
 export default function EditFestival() {
+  const [dataBeforeUpdate, setDataBeforeUpdate] = useState(null);
+  const navigate = useNavigate();
+  const { showSnackbar, Snackbar } = useSnackbar();
+  const id = useParams().id;
+
+  const handleEditFestival = async (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const formDataJson = Object.fromEntries(formData.entries());
+    if (!formDataJson.file.size) {
+      formDataJson.file = null;
+    }
+    const config = {
+      headers: {
+        "content-type": "multipart/form-data",
+      },
+    };
+    showSnackbar(true, "Loading...");
+    try {
+      const { data } = await axios.put(
+        `https://be-pariwisata-aceh.vercel.app/festival/${id}`,
+        formDataJson,
+        config
+      );
+      showSnackbar(true, data.message[0]);
+      setTimeout(() => {
+        showSnackbar(false, null);
+        navigate("/admin/festival");
+      }, 1000);
+    } catch (error) {
+      console.log(error);
+      showSnackbar(true, error.response.data.message[0]);
+      setTimeout(() => {
+        showSnackbar(false, null);
+      }, 2000);
+    }
+  };
+
+  const getFestivalFromApi = async () => {
+    showSnackbar(true, "get data...");
+    try {
+      const { data } = await axios.get(
+        `https://be-pariwisata-aceh.vercel.app/festival/${id}`
+      );
+      showSnackbar(false, null);
+      setDataBeforeUpdate(data.data);
+    } catch (error) {
+      showSnackbar(true, error.response.data.message[0]);
+      setTimeout(() => {
+        showSnackbar(false, null);
+      }, 2000);
+    }
+  };
+
+  useEffect(() => {
+    getFestivalFromApi();
+  }, []);
+
   return (
     <>
+      <Snackbar />
       <NavigationAdmin>
-        <div className="pt-28 px-20 ">
+        <form onSubmit={handleEditFestival} className="pt-28 px-20 ">
           <HeadingAdmin text="Edit Festival" />
           <div className="flex flex-col gap-2">
-            <InputAdmin name="input" type="text" placeholder="masjid raya" />
-            <InputAdmin name="input" type="text" placeholder="Judul Berita" />
+            <InputAdmin
+              name="title"
+              type="text"
+              placeholder="Nama Festival"
+              defaultValue={dataBeforeUpdate?.title}
+            />
+            <InputAdmin
+              name="locate"
+              type="text"
+              placeholder="Lokasi"
+              defaultValue={dataBeforeUpdate?.locate}
+            />
             <InputAdmin
               name="input"
               type="date"
-              placeholder="Tanggal festival"
+              placeholder="Tanggal Mulai"
+              defaultValue={dataBeforeUpdate?.startDate}
             />
-            <InputAdmin name="input" type="date" placeholder="Tanggal Upload" />
-            <TextareaAdmin id={1} name="date" placeholder="Deskripsi" />
+            <InputAdmin
+              name="input"
+              type="date"
+              placeholder="Tanggal Berakhir"
+              defaultValue={dataBeforeUpdate?.endDate}
+            />
+            <TextareaAdmin
+              id={1}
+              name="desc"
+              placeholder="Deskripsi"
+              defaultValue={dataBeforeUpdate?.desc}
+            />
           </div>
           <div className="flex justify-center items-center my-10">
             <button
               className="bg-yellow-primary text-white py-2 px-4 rounded-lg"
               name="button"
-              type="button"
-              // onClick={onClick}
+              type="submit"
             >
-              Simpan
+              Update
             </button>
           </div>
-        </div>
+        </form>
       </NavigationAdmin>
     </>
   );
--- src/pages/admin/festival/tambah-festival.jsx
+++ src/pages/admin/festival/tambah-festival.jsx
@@ -3,35 +3,96 @@ import TextareaAdmin from "../../../components/micro/TextareaAdmin";
 import NavigationAdmin from "../../../components/NavigationAdmin";
 import InputAdmin from "../../../components/micro/InputAdmin";
 import HeadingAdmin from "../../../components/micro/HeadingAdmin";
+import axios from "axios";
+import useSnackbar from "../../../hooks/useSnackbar";
+import { useNavigate } from "react-router-dom";
 
 export default function TambahFestival() {
+  const navigate = useNavigate();
+  const { showSnackbar, Snackbar } = useSnackbar();
+  const handleAddFestival = async (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const formDataJson = Object.fromEntries(formData.entries());
+    // if (!formDataJson.file.size) {
+    //   setTimeout(() => {
+    //     showSnackbar(false, null);
+    //   }, 2000);
+    //   return;
+    // }
+    const config = {
+      headers: {
+        "content-type": "multipart/form-data",
+      },
+    };
+    showSnackbar(true, "Loading...");
+    try {
+      const { data } = await axios.post(
+        `https://be-pariwisata-aceh.vercel.app/festival/add`,
+        formDataJson,
+        config
+      );
+      showSnackbar(true, data.message[0]);
+      setTimeout(() => {
+        showSnackbar(false, null);
+        navigate("/admin/festival");
+      }, 1000);
+    } catch (error) {
+      console.log(error);
+      showSnackbar(true, error.response.data.message[0]);
+      setTimeout(() => {
+        showSnackbar(false, null);
+      }, 2000);
+    }
+  };
+
+  // const onHandleInputFileChange = (event) => {
+  //   if (event.target.files) {
+  //     const file = event.target.files[0];
+  //     const reader = new FileReader();
+  //     reader.onload = (e) => {
+  //       setSelectedImage(e.target.result);
+  //     };
+  //     reader.readAsDataURL(file);
+  //   } else {
+  //     setTimeout(() => {
+  //       showSnackbar(false, null);
+  //     }, 2000);
+  //   }
+  // };
+
   return (
     <>
+      <Snackbar />
       <NavigationAdmin>
-        <div className="pt-28 px-20 ">
+        <form onSubmit={handleAddFestival} className="pt-28 px-20 ">
           <HeadingAdmin text="Tambah Festival" />
           <div className="flex flex-col gap-2">
-            <InputAdmin name="input" type="text" placeholder="masjid raya" />
-            <InputAdmin name="input" type="text" placeholder="Judul Berita" />
+            <InputAdmin name="title" type="text" placeholder="Nama Festival" />
+            <InputAdmin name="locate" type="text" placeholder="Lokasi" />
             <InputAdmin
-              name="input"
+              name="startDate"
               type="date"
-              placeholder="Tanggal festival"
+              placeholder="Tanggal Mulai"
             />
-            <InputAdmin name="input" type="date" placeholder="Tanggal Upload" />
-            <TextareaAdmin id={1} name="date" placeholder="Deskripsi" />
+            <InputAdmin
+              name="endDate"
+              type="date"
+              placeholder="Tanggal Berakhir"
+            />
+            <TextareaAdmin id={1} name="desc" placeholder="Deskripsi" />
           </div>
           <div className="flex justify-center items-center my-10">
             <button
               className="bg-yellow-primary text-white py-2 px-4 rounded-lg"
               name="button"
-              type="button"
+              type="submit"
               // onClick={onClick}
             >
               Simpan
             </button>
           </div>
-        </div>
+        </form>
       </NavigationAdmin>
     </>
   );
